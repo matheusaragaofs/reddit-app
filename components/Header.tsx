dcd61d679d2235d6497e1ca7a1e3d6340bb50e3d
matheus_aragao <matheusaragaofs@gmail.com>
2022-11-17 21:19:16 -0300

    building the header component

---
+++ components/Header.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Image from 'next/image'
+import { BeakerIcon, ChevronDownIcon, HomeIcon, MenuIcon, SearchIcon } from '@heroicons/react/solid'
+import { StarIcon, BellIcon, ChatIcon, PlusIcon, SparklesIcon, SpeakerphoneIcon, VideoCameraIcon, GlobeIcon } from '@heroicons/react/outline'
+const Header = () => {
+  return (
+    <div className='sticky top-0 flex z-50 bg-white px-4 py-2 shadow-sm'>
+      <div className='relative h-10 w-20 flex-shrink-0 cursor-pointer'>
+        <Image
+          style={{ objectFit: 'contain', }}
+          src="https://logos-world.net/wp-content/uploads/2020/10/Reddit-Logo.png"
+          alt='image'
+          fill
+        />
+      </div>
+
+      <div className='flex items-center mx-7 xl:min-w-[300px]'>
+        <HomeIcon className='h-5 w-5' />
+        <p className='flex-1 hidden lg:inline'>Home</p>
+        <ChevronDownIcon className='h-5 w-5' />
+      </div>
+
+      {/* Search Box */}
+      <form className='flex flex-1 items-center space-x-2 border border-gray-200 bg-gray-100 px-3 py-1 rounded-sm' action="">
+        <SearchIcon className='h-6 w-6 text-gray-400' />
+        <input className='outline-none bg-gray-100 flex-1' type="text" placeholder='Search Reddit' />
+        <button hidden type='submit' />
+      </form>
+
+      <div className='text-gray-500 space-x-2 items-center hidden lg:inline-flex '>
+        <SparklesIcon className='icon'/>
+        <GlobeIcon className='icon'/>
+        <VideoCameraIcon className='icon'/>
+        <hr className='h-10 border boder-gray-100' />
+        <ChatIcon className='icon'/>
+        <BellIcon className='icon'/>
+        <PlusIcon className='icon'/>
+        <SpeakerphoneIcon className='icon'/>
+      </div>
+
+      <div className='ml-5 flex items-center lg:hidden'>
+        <MenuIcon className='icon'/>
+      </div>
+    </div>
+  )
+}
+
+export default Header
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
-        <title>Create Next App</title>
+        <title>Reddit App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-        <h1>Hello world</h1>
-      
-    </div>
+      <Header/>
+    </div> 
   )
 }
 
